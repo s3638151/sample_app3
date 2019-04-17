219b0e2392383b40fe40e7f080ecdebee24ae6c2
CANDICE123DUO <you@s3638151@student.rmit.edu.au>
2019-04-17 11:40:13 +0000

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,11 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get home" do
-    get root_path
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
 
   test "should get help" do
     get help_path
